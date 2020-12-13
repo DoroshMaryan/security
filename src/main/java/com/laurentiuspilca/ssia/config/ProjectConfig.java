e9027be0ed6c80671ff6ee77a10bd56903af46d4
Maryan Dorosh <Maryan_Dorosh@epam.com>
2020-12-13 15:42:01 +0200

    Listing 2.12 Configuring in-memory user management

--- src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
+++ src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -2,6 +2,7 @@ package com.laurentiuspilca.ssia.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -28,14 +29,14 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
         return userDetailsService;
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+        .withUser("maryan")
+        .password("1111")
+        .authorities("read")
+        .and()
+        .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();
-        http.authorizeRequests().anyRequest().authenticated();
-    }
 }
