cf5645c93c15354bfb1fcee265efb6f505fe3f93
Maryan Dorosh <Maryan_Dorosh@epam.com>
2020-12-14 20:27:22 +0200

    [Ex 3.2] Configuring jdbc userDetails service

--- src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
+++ src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -1,31 +1,26 @@
 package com.laurentiuspilca.ssia.config;
 
-import com.laurentiuspilca.ssia.services.InMemoryUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import java.util.List;
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
-    @Override
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("maryan")
-                .password("1111")
-                .authorities("read")
-                .build();
-        List<UserDetails> users = List.of(user);
-
-
-        return new InMemoryUserDetailsService(users);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        String usersByUsernameQuery = "select username, password, enabled from spring_security.users where username = ?";
+        String authsByUserQuery = "select username, authority from spring_security.authorities where username = ?";
+        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+        return userDetailsManager;
     }
 
     @Bean
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `spring_security`.`users` VALUES (NULL, 'john', '1234', '1');
+INSERT INTO `spring_security`.`users` VALUES (NULL, 'maryan', '1111', '1');
+
+INSERT INTO `spring_security`.`authorities` VALUES (NULL, 'john', 'write');
+INSERT INTO `spring_security`.`authorities` VALUES (NULL, 'maryan', 'read');
