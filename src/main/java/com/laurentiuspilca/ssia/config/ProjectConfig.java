4d3d872c7102e4a5fa4b85cd01967bb889f25e76
Maryan Dorosh <Maryan_Dorosh@epam.com>
2020-12-22 14:47:37 +0200

    [Ex 5.3] Add AuthenticationEntryPoint

--- src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
+++ src/main/java/com/laurentiuspilca/ssia/config/ProjectConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -36,6 +37,15 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) {
         auth.authenticationProvider(authenticationProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic(c -> {
+            c.realmName("OTHER");
+            c.authenticationEntryPoint(new CustomEntryPoint());
+        });
+        http.authorizeRequests().anyRequest().authenticated();
     }
 
     @Bean
--- src/main/java/com/laurentiuspilca/ssia/controllers/HelloController.java
+++ src/main/java/com/laurentiuspilca/ssia/controllers/HelloController.java
@@ -1,6 +1,8 @@
 package com.laurentiuspilca.ssia.controllers;
 
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
+import org.springframework.security.concurrent.DelegatingSecurityContextExecutorService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -51,7 +53,29 @@ public class HelloController {
         ExecutorService executorService = Executors.newCachedThreadPool();
 
         try {
-            return executorService.submit(task).get();
+//            DelegatingSecurityContextCallable<String> delegatingSecurityContextCallable = new DelegatingSecurityContextCallable<>(task);
+            var delegatingSecurityContextCallable = new DelegatingSecurityContextCallable<>(task);
+            return String.format("Callable %s", executorService.submit(delegatingSecurityContextCallable).get());
+        } finally {
+            executorService.shutdown();
+        }
+    }
+
+    @GetMapping("callable-executor")
+    public String callExecutor() throws Exception {
+        System.out.println(Thread.currentThread().getName());
+        Callable<String> task = () -> {
+            System.out.println(Thread.currentThread().getName());
+            System.out.println("start task");
+            final SecurityContext context = SecurityContextHolder.getContext();
+            return context.getAuthentication().getName();
+        };
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService = new DelegatingSecurityContextExecutorService(executorService);
+
+        try {
+            return String.format("Callable executor %s", executorService.submit(task).get());
         } finally {
             executorService.shutdown();
         }
