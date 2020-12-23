b2964264de1178c0f532e38dd20dc784a989631f
Maryan Dorosh <Maryan_Dorosh@epam.com>
2020-12-23 15:13:22 +0200

    [Ex 6.1.1] Use DelegatingPasswordEncoder

--- src/main/java/com/laurentiuspilca/ssia/security/AuthenticationProviderService.java
+++ src/main/java/com/laurentiuspilca/ssia/security/AuthenticationProviderService.java
@@ -4,15 +4,17 @@ import com.laurentiuspilca.ssia.details.CustomUserDetails;
 import com.laurentiuspilca.ssia.entities.enums.EncryptionAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 public class AuthenticationProviderService implements AuthenticationProvider {
@@ -26,27 +28,24 @@ public class AuthenticationProviderService implements AuthenticationProvider {
     @Autowired
     private SCryptPasswordEncoder sCryptPasswordEncoder;
 
+
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         final String userName = authentication.getName();
         final String password = authentication.getCredentials().toString();
 
+        Map<String, PasswordEncoder> idToPasswordEncoder = Map.of(EncryptionAlgorithm.BCRYPT.name(), bCryptPasswordEncoder,
+                EncryptionAlgorithm.SCRYPT.name(), sCryptPasswordEncoder);
+
         System.out.println("CustomAuthenticationProvider.authenticate");
 
         final CustomUserDetails userDetails = userDetailsService.loadUserByUsername(userName);
+        DelegatingPasswordEncoder delegatingPasswordEncoder;
+        delegatingPasswordEncoder = new DelegatingPasswordEncoder(userDetails.getUser().getAlgorithm().name(), idToPasswordEncoder);
 
-        if (userDetails.getUser().getAlgorithm().equals(EncryptionAlgorithm.BCRYPT)) {
-            return checkPassword(userDetails, password, bCryptPasswordEncoder);
-        } else if (userDetails.getUser().getAlgorithm().equals(EncryptionAlgorithm.SCRYPT)) {
-            return checkPassword(userDetails, password, sCryptPasswordEncoder);
-        } else {
-            throw new AuthenticationServiceException("bad EncryptionAlgorithm");
-        }
-    }
-
-    private Authentication checkPassword(final CustomUserDetails user, final String rawPassword, final PasswordEncoder encoder) {
-        if (encoder.matches(rawPassword, user.getPassword())) {
-            return new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), user.getAuthorities());
+        if (delegatingPasswordEncoder.matches(password, userDetails.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
         } else {
             throw new BadCredentialsException("Bad credentials");
         }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT IGNORE INTO `spring_security`.`user` (`id`, `username`, `password`, `algorithm`) VALUES ('1', 'john', '$2a$10$Ev8DTnRoA4DfuAn7tbge5OvTjtFYIv8pc7IEL26WRJfEiHDjxVc56', 'BCRYPT');
+INSERT IGNORE INTO `spring_security`.`user` (`id`, `username`, `password`, `algorithm`) VALUES ('1', 'john', '{BCRYPT}$2a$10$Ev8DTnRoA4DfuAn7tbge5OvTjtFYIv8pc7IEL26WRJfEiHDjxVc56', 'BCRYPT');
 
 INSERT IGNORE INTO `spring_security`.`authority` (`id`, `name`, `user`) VALUES ('1', 'READ', '1');
 INSERT IGNORE INTO `spring_security`.`authority` (`id`, `name`, `user`) VALUES ('2', 'WRITE', '1');
